33daab2426f2b349c125d9aa9c57ac2923a1b900
leaftha <vltpcks@gmail.com>
2020-07-04 02:48:25 +0900

    day2-clock

---
+++ day2/js/day2.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const senconds = now.getSeconds();
+  const sencondDegress = (senconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${sencondDegress}deg)`;
+
+  const min = now.getUTCMinutes();
+  const minDegress = (min / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minDegress}deg)`;
+
+  const hour = now.getHours();
+  const hourDegress = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegress}deg)`;
+}
+
+setInterval(setDate, 1000);
