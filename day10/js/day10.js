66bdb16dbc7ee258112275a2bcb3cd97baae4a69
leaftha <vltpcks@gmail.com>
2020-07-11 03:14:03 +0900

    day10 Multiple Checkboxes

---
+++ day10/js/day10.js
@@ -0,0 +1,26 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(event) {
+  let inBetween = false;
+  if (event.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      console.log(checkboxes);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log("Starting to check them in between!");
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
